d74a715d47b791dbbe68594b655763a25dac8703
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-06-30 20:27:25 +0200

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+# Using Puppet install flask from pip3
+
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
