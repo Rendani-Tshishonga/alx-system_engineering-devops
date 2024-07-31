45dfbf973cc1c2911410d3ab0b313d1bab6daec2
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-31 14:50:07 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import re
 
 API = 'https://jsonplaceholder.typicode.com'
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             user_id = int(sys.argv[1])
@@ -15,7 +15,7 @@ if __name__ =="__main__":
             tasks = requests.get('{}/todos'.format(API)).json()
             number_of_tasks = list(filter(lambda x: x.get('userId') == user_id, tasks))
             number_of_completed = list(filter(lambda x: x.get('completed'), number_of_tasks))
-            print('Employee {} is done with tasks ({}/{}):'.format(
+            print('Employee {} is done with tasks ({}/{}):' .format(
                 employee_name,
                 len(number_of_completed),
                 len(number_of_tasks)))
